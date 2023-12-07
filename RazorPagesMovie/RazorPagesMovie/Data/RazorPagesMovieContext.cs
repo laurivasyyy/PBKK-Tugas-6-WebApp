ebe53f880cdcd48b98cb1c7e67fc0e77cbbd40cb
laurivasyyy <jalusakti56@gmail.com>
2023-12-07 14:41:15 +0700

    Initial Commit

---
+++ RazorPagesMovie/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data;
+
+public class RazorPagesMovieContext : DbContext
+{
+    public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movie => Set<Movie>();
+}
